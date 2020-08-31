60cdebad11ef323434cdd1dce6b8cd0d1c63e657
John Poulin <john.m.poulin@gmail.com>
2020-08-31 19:31:01 -0400

    Able to dump secret content to file, as an example

--- thycotic_vault/recipes/default.rb
+++ thycotic_vault/recipes/default.rb
@@ -11,5 +11,15 @@ gem_package 'dsv-sdk' do
 end
 
 dsv_credential 'cred' do
-    action :create
+    client_id 'CLIENT_ID'
+    client_secret 'CLIENT_SECRET'
+    tenant 'tmg'
+    tld 'com'
+    query '/test/sdk/simple'
+end
+
+file '/tmp/test.txt' do
+	sensitive true
+	content lazy { node.run_state['cred']["data"]["password"] }
+	only_if { node.run_state.key?('cred') }
 end
--- thycotic_vault/resources/dsv_credential.rb
+++ thycotic_vault/resources/dsv_credential.rb
@@ -3,10 +3,26 @@ require 'dsv'
 resource_name :dsv_credential
 provides :dsv_credential
 
+property :name, String, name_property: true
+property :client_id, String
+property :client_secret, String
+property :tenant, String
+property :tld, String, default: "com"
+property :query, String
+
 action :create do
   begin
-    v = Dsv::Vault.new
-  rescue
-    raise "Could not find credential matching query: #{new_resource.query}!"
+    v = Dsv::Vault.new(
+      client_id: new_resource.client_id,
+      client_secret: new_resource.client_secret,
+      tenant: new_resource.tenant,
+      tld: new_resource.tld
+    )
+
+    secret = Dsv::Secret.fetch(v, new_resource.query)
+
+    node.run_state[new_resource.name] = secret
+  rescue Exception => e
+    raise "Could not find credential matching query!"
   end
 end
