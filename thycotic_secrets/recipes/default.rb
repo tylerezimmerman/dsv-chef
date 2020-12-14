607de2ae477d73f590f1838f9294293488be9cc1
endlesstrax <ricky@whitelionmedia.com>
2020-12-14 13:37:44 -0500

    Updated to take client secrets from a `knife data bag`.

--- thycotic_secrets/recipes/default.rb
+++ thycotic_secrets/recipes/default.rb
@@ -12,29 +12,33 @@ gem_package 'tss-sdk' do
     version '0.0.1'
 end
 
+dsv_secrets = data_bag_item('thycotic', 'thycotic_dsv')
+
 dsv_secret 'dsv-secret' do
-    client_id 'af8eb095-bdac-4d5a-9b4d-632c5bf7009c'
-    client_secret 'secret'
-    tenant 'tmg'
-    tld 'com'
-    secret_path '/test/secret'
+    client_id       dsv_secrets["thycotic_client_id"]
+    client_secret   dsv_secrets["thycotic_client_secret"]
+    tenant          dsv_secrets["thycotic_tenant"]
+    tld             'com'
+    secret_path     dsv_secrets["thycotic_secret_path"]
 end
 
+tss_secrets = data_bag_item('thycotic', 'thycotic_tss')
+
 tss_secret 'tss-secret' do
-    username 'sdk_test_app'
-    password 'password'
-    tenant 'tmg'
-    secret_id '1'
+    username    tss_secrets["thycotic_username"]
+    password    tss_secrets["thycotic_password"]
+    tenant      tss_secrets["thycotic_tenant"]
+    secret_id   tss_secrets["thycotic_secret_id"]
 end
 
 file '/tmp/dsv-test.txt' do
 	sensitive true
-	content lazy { node.run_state['dsv-secret']["data"]["password"] }
+	content lazy { node.run_state['dsv-secret'].to_s }
 	only_if { node.run_state.key?('dsv-secret') }
 end
 
 file '/tmp/tss-test.txt' do
 	sensitive true
-	content lazy { node.run_state['tss-secret']["data"] }
+	content lazy { node.run_state['tss-secret'].to_s }
 	only_if { node.run_state.key?('tss-secret') }
 end
